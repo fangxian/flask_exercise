c6c93610d8c54d00a7d69da85ac5d6a07ed1d335
fangxian <fangxian1108@gmail.com>
2018-05-16 22:12:28 +0800

    update

--- flask1/flask1.py
+++ flask1/flask1.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm
+from models import User
+from flask_login import login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'register page'
@@ -11,7 +14,20 @@ def hello_world():
 
 @app.route('/register')
 def register():
-    pass
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(form.email.data).first()
+        if user is None:
+            user.email = form.email.data
+            user.username = form.username.data
+            user.password = form.password.data
+            db.session.add(user)
+            db.session.commit()
+            flash(u'注册成功')
+            return redirect(url_for('hello_world'))
+        flash('email has been registered')
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run()
