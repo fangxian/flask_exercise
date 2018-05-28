7805f0d5421d6aec1b16a7c036c04689ffc07ab5
fangxian <fangxian1108@gmail.com>
2018-05-28 22:09:26 +0800

    update

--- mov/app/__init__.py
+++ mov/app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import FlaskRedis
 import pymysql
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://root:root@localhost:3306/movie"
+app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://root:root@127.0.0.1:3306/movie"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = "movie app"
 app.config["REDIS_URL"] = "redis://192.168.4.1:6379/0"
+app.config['UP'] = os.path.join(os.path.dirname(__file__), "static/uploads")
 app.debug = True
 db = SQLAlchemy(app)
 rd = FlaskRedis(app)
